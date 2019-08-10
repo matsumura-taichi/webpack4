09419fdc4a330ce0ed5a7fa5acb7e161ea6b1f92
松村 泰地 <matsumura.taichi@a-tm.co.jp>
2019-08-10 14:58:42 +0900

    add index.js and sub.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// importでsub.jsを読み込み
+import { hello } from "./sub";
+
+// sub.jsに定義されたhello関数を実行
+hello();
---
+++ src/sub.js
@@ -0,0 +1,4 @@
+// exportでhello関数を定義
+export function hello() {
+	alert("helloメソッドが実行");
+}
